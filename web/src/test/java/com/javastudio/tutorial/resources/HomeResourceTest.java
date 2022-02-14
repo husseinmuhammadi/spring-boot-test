33e1c9a7c238b42e1a533eee1dba67bf4abc9dc9
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-02-14 15:14:32 +0100

    MockMvc

---
+++ web/src/test/java/com/javastudio/tutorial/resources/HomeResourceTest.java
@@ -0,0 +1,28 @@
+package com.javastudio.tutorial.resources;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HomeResourceTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnItWorks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/index"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("It works!")));
+    }
+}
