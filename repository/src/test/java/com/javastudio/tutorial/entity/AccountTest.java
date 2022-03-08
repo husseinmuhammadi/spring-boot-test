037176547531450ab915a3233dc86f0e0d234a15
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-03-08 13:04:12 +0100

    Entity id with generated value Entity with the generated value will save with new value even it has the value

--- repository/src/test/java/com/javastudio/tutorial/entity/AccountTest.java
+++ repository/src/test/java/com/javastudio/tutorial/entity/AccountTest.java
@@ -22,14 +22,16 @@ class AccountTest {
     @Test
     void whenSaveAccount_thenIdShouldBeGenerated() {
         Account account = new Account("NL89RABO0123456789");
-        Assertions.assertThat(accountRepository.save(account).getId()).isNotNull();
+        Assertions.assertThat(accountRepository.save(account).getId())
+                .isNotNull();
     }
 
     @Test
-    void name() {
+    void givenEntityIdWithGeneratedValue_whenSaveEntityWithPreSetId_thenIdWillGenerated() {
         UUID accountId = UUID.randomUUID();
         Account account = new Account("NL89RABO0123456789");
         account.setId(accountId);
-//        Assertions.assertThat(accountRepository.save(account).getId()).isEqualTo(accountId);
+        Assertions.assertThat(accountRepository.save(account).getId())
+                .isNotEqualTo(accountId);
     }
 }
