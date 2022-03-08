b89ad7547d6adf363fa1927bdca89c5a781b572f
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-03-08 14:30:12 +0100

    Automatic Rollback of Transactions in Spring Tests

--- repository/src/test/java/com/javastudio/tutorial/repository/ProductRepositoryTest.java
+++ repository/src/test/java/com/javastudio/tutorial/repository/ProductRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.javastudio.tutorial.repository;
 
+import com.javastudio.tutorial.entity.Product;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,8 +16,26 @@ class ProductRepositoryTest {
     @Autowired
     private EntityManager em;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
     void shouldLoadEntityManagerInContext() {
         assertNotNull(em);
+    }
+
+    @Test
+    void shouldGenerateNewId() {
+        Assertions.assertThat(productRepository.save(new Product("Laptop")).getId()).isNotNull();
+    }
+
+    @Test
+    void shouldSaveWithNewGeneratedId() {
+        Product product = new Product();
+        product.setId(100L);
+        product.setName("Laptop");
+
+        Assertions.assertThat(productRepository.save(product).getId()).isEqualTo(1L);
+        Assertions.assertThat(productRepository.save(product).getId()).isNotEqualTo(100L);
     }
 }
